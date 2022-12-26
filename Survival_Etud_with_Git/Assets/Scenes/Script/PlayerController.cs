5230c12d5fb731aed0e000b16bb088880cc7d58d
LeventBilgin <LeventBilginDeveloper@Gmail.com>
2022-12-26 23:53:33 +0300

    Ilk Player Script Yazildi..
    Test Amacli Dummy Bir Script Olusturuldu..

---
+++ Survival_Etud_with_Git/Assets/Scenes/Script/PlayerController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace scenes.script
+{
+    public class PlayerController : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        private  void Awake()
+        {
+        // player control referance'lari cache'ler
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        // player controller refererance'larini cachelendigi degiskenleri kullanir.
+        }
+    }
+
+}
