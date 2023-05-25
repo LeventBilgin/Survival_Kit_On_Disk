188e081cfb4a0e617db1f2ace9d4ba18dc1e62e5
LeventBilgin <LeventBilgin@KiddingBox.com>
2023-05-26 00:32:40 +0300

    Item.cs ve Metalari Eklendi

---
+++ Survival_Etud_with_Git/Assets/Scenes/Script/Item.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Scenes.Script
+{
+    [System.Serializable]
+    public class Item : MonoBehaviour
+    {
+        public string name;
+        public int id;
+
+        
+        
+        
+       
+    }
+
+}
